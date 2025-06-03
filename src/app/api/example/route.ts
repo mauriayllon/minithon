5c407406fdb5e8830abcf2c00ce3988e53d803a0
mauriayllon <mauricio.ad1707@gmail.com>
2025-06-03 18:48:16 -0400

    option cors

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -102,3 +102,15 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: 'Error Interno del Servidor' }, { status: 500 });
   }
 }
+
+export async function OPTIONS(request: NextRequest) {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version',
+    },
+  });
+}
